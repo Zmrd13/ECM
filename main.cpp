65c6c64c6d723a366fbcffd7252476026a331796
Александров Михаил Александрович <ip913s02@mwp06.csc.local>
2021-04-19 11:59:34 +0700

    ui tesw

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ int operand = 5;
 int value = 0;
 int encode = 0;
 void sc_memoryShow(){
+
     mt_gotoXY(0,0);
+    cout<<endl;
     for(int i=0;i<32;i++){
         cout<<"_";
     }
@@ -31,7 +33,7 @@ void sc_memoryShow(){
     cout<<"|";
 }
 void sc_accShow(){
-    mt_gotoXY(2,73);
+    mt_gotoXY(0,73);
     for(int i=0;i<6;i++){
         cout<<"_";
     }
@@ -39,6 +41,70 @@ void sc_accShow(){
     for(int i=0;i<6;i++){
         cout<<"_";
     }
+    mt_gotoXY(2,73);
+    cout<<"|     TEST     |";
+    mt_gotoXY(3,73);
+    cout<<"|";
+    for(int i=0;i<14;i++){
+        cout<<"_";
+    }
+    cout<<"|";
+}
+void sc_insShow(){
+    mt_gotoXY(4,73);
+    for(int i=0;i<3;i++){
+        cout<<"_";
+    }
+    cout<<"InsCount";
+    for(int i=0;i<4;i++){
+        cout<<"_";
+    }
+    mt_gotoXY(5,73);
+    cout<<"|     TEST     |";
+    mt_gotoXY(6,73);
+    cout<<"|";
+    for(int i=0;i<14;i++){
+        cout<<"_";
+    }
+    cout<<"|";
+}
+
+void sc_operShow(){
+    mt_gotoXY(7,73);
+    for(int i=0;i<3;i++){
+        cout<<"_";
+    }
+     cout<<"Operation";
+    for(int i=0;i<3;i++){
+        cout<<"_";
+    }
+    
+    mt_gotoXY(8,73);
+    cout<<"|     TEST     |";
+    mt_gotoXY(9,73);
+    cout<<"|";
+    for(int i=0;i<14;i++){
+        cout<<"_";
+    }
+    cout<<"|";
+}
+void sc_flagShow(){
+    mt_gotoXY(10,73);
+    for(int i=0;i<3;i++){
+        cout<<"_";
+    }
+    cout<<"Flags___";
+    for(int i=0;i<4;i++){
+        cout<<"_";
+    }
+    mt_gotoXY(11,73);
+    cout<<"|     TEST     |";
+    mt_gotoXY(12,73);
+    cout<<"|";
+    for(int i=0;i<14;i++){
+        cout<<"_";
+    }
+    cout<<"|";
 }
 int main() {
     mt_clrscr();
@@ -48,10 +114,12 @@ int main() {
     sc_memoryLoad(filename);
     sc_memorySet(4,16);
     sc_memoryShow();
-     sc_accShow();
-    mt_gotoXY(2,73);
-
-
+    sc_accShow();
+    
+    sc_insShow();
+    sc_operShow();
+    sc_flagShow();
+    mt_gotoXY(20,0);
 
 
 
